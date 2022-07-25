6619da3b41aaacbaff6971ac409e2e41ddb95951
gustavo lemos <gustavolemosmendes@gmail.com>
2022-07-25 20:31:31 -0300

    Feat: Criando interfaces

---
+++ src/screens/Register.tsx
@@ -0,0 +1,26 @@
+import { Input, VStack } from 'native-base';
+import { Button } from '../components/Button';
+import { Header } from '../components/Header';
+
+export function Register() {
+  return (
+    <VStack flex={1} p={6} bg='gray.600'>
+        <Header title='Nova solicitação'/>
+        <Input 
+            placeholder='Número do patrimônio'
+            mt={4}
+        />
+        <Input 
+            placeholder='Descricao do problema'
+            flex={1}
+            mt={5}
+            multiline
+            textAlignVertical='top'
+        />
+        <Button 
+            title='Cadastrar'
+            mt={5}
+        />
+    </VStack>
+  );
+}
