b210979c4392a5c3fd8c934ff0cc3819f50decfd
gustavo lemos <gustavolemosmendes@gmail.com>
2022-07-25 01:16:20 -0300

    Fix: usando o componente de input que eu criei

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,8 +1,9 @@
-import { Heading, Input, VStack, Icon, useTheme } from "native-base";
+import { Heading, VStack, Icon, useTheme } from "native-base";
 import Logo from '../assets/logo_primary.svg'
 import { Envelope, Key } from 'phosphor-react-native'
-import { Button } from "../components/button";
+import { Button } from "../components/Button";
 import { useState } from "react";
+import { Input } from "../components/Input";
 
 export function SignIn() {
 
